85167c5008082731b8eb580b7ec9584e158646c9
Maya Oniciuc <maya.klmno@gmail.com>
2025-03-17 09:54:17 -0700

    Initial commit

---
+++ app/src/main/java/com/example/myharrypotterapp/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.myharrypotterapp
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    // here you should set up the binding and the adaptor
+    //TODO: make the adaptor and then write the private lateninit here
+    //private lateinit var binding: ActivityMainBinding
+
+    companion object{
+        val TAG = ""
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
