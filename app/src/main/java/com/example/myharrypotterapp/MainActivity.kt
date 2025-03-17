af224847baada8bf0b1728aefd206a4e4ee581ec
Maya Oniciuc <maya.klmno@gmail.com>
2025-03-17 10:05:37 -0700

    March 17, 2025 (class time) - This is what I did during the class time, but there are still quite a lot of errors. And I am still on the beginning.

--- app/src/main/java/com/example/myharrypotterapp/MainActivity.kt
+++ app/src/main/java/com/example/myharrypotterapp/MainActivity.kt
@@ -10,19 +10,23 @@ class MainActivity : AppCompatActivity() {
     // here you should set up the binding and the adaptor
     //TODO: make the adaptor and then write the private lateninit here
     //private lateinit var binding: ActivityMainBinding
+    private lateinit var binding: MainActivityBinding
 
     companion object{
-        val TAG = ""
+        val TAG = "MainActivity"
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        binding = MainActivityBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(binding.root) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+
     }
 }
