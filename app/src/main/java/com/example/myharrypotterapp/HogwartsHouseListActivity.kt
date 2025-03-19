291b2af3f4e2760e223657e446e795ee82dc791b
Maya Oniciuc <maya.klmno@gmail.com>
2025-03-19 10:25:11 -0700

    March 19, 2025 (class time) - trying to get some things to work

--- app/src/main/java/com/example/myharrypotterapp/HogwartsHouseListActivity.kt
+++ app/src/main/java/com/example/myharrypotterapp/HogwartsHouseListActivity.kt
@@ -6,11 +6,11 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class HogwartsHouseListActivity : AppCompatActivity() {
     // here you should set up the binding and the adaptor
     //TODO: make the adaptor and then write the private lateninit here
     //private lateinit var binding: ActivityMainBinding
-    private lateinit var binding: MainActivityBinding
+    private lateinit var binding: HogwartsHouseListActivity
 
     companion object{
         val TAG = "MainActivity"
@@ -19,7 +19,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        binding = MainActivityBinding.inflate(layoutInflater)
+        binding = HogwartsHouseListActivity.inflate(layoutInflater)
         setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(binding.root) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
@@ -27,6 +27,7 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
-
+       // val sortingHatService = RetrofitHelper.getInstance().create(sortingHatService::class.java)
+        // val sortingHatCall = sortingHatService.getAllDaySortingHatData()
     }
 }
