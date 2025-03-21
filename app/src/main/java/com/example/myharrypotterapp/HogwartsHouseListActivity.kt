f2ddabd014b2f707c8d88d92b6b450bb008c60b0
Maya Oniciuc <maya.klmno@gmail.com>
2025-03-21 09:37:54 -0700

    March 21, 2025 (class time) - got the binding to not be red

--- app/src/main/java/com/example/myharrypotterapp/HogwartsHouseListActivity.kt
+++ app/src/main/java/com/example/myharrypotterapp/HogwartsHouseListActivity.kt
@@ -5,12 +5,13 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.myharrypotterapp.databinding.HogwartsHouseListBinding
 
 class HogwartsHouseListActivity : AppCompatActivity() {
     // here you should set up the binding and the adaptor
     //TODO: make the adaptor and then write the private lateninit here
     //private lateinit var binding: ActivityMainBinding
-    private lateinit var binding: HogwartsHouseListActivity
+    private lateinit var binding: HogwartsHouseListBinding
 
     companion object{
         val TAG = "MainActivity"
@@ -19,7 +20,7 @@ class HogwartsHouseListActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        binding = HogwartsHouseListActivity.inflate(layoutInflater)
+        binding = HogwartsHouseListBinding.inflate(layoutInflater)
         setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(binding.root) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
@@ -27,7 +28,17 @@ class HogwartsHouseListActivity : AppCompatActivity() {
             insets
         }
 
-       // val sortingHatService = RetrofitHelper.getInstance().create(sortingHatService::class.java)
-        // val sortingHatCall = sortingHatService.getAllDaySortingHatData()
+        val sortingHatService = RetrofitHelper.getInstance().create(sortingHatService::class.java)
+        val sortingHatCall = sortingHatService.getAllDaySortingHatData()
+
+        sortingHatCall.enqueue(object: Callback<FeatureCollection> {
+            override fun onResponse(call: Call<FeatureCollection>, response: Response<FeatureCollection>){
+                // TODO: this is where the code goes for when you get your data
+                // create your recyclerView adapeter HERE
+                response.body()
+                val sortingHat = response.body()!!
+                //adapter = SortingHatAdapter(sortingHat.)
+            }
+        })
     }
 }
